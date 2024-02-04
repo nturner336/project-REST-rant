c66b9d3287696068c2e1d67e354362c6c5edbbc4
Nichole Turner <nicholeturner@Nicholes-Air.lan>
2024-02-04 13:14:51 -0500

    Navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const NewForm = require('./NewForm'); // Adjust the import path accordingly
+
 
 let places = [
   {
--- views/default.jsx
+++ views/default.jsx
@@ -3,12 +3,22 @@ const React = require('react')
 function Def (html) {
     return (
         <html>
-            <head>
-                <title>Title</title>
-            
-                <link rel="stylesheet" href="/css/style.css"/>
-            </head>
+            <head>...</head>
             <body>
+            <nav>
+                <ul>
+                    <li>
+                    <a href="/">Home</a>
+                    </li>
+                    <li>
+                    <a href="/places">Places</a>
+                    </li>
+                    <li>
+                    <a href="/places/new">Add Place</a>
+                    </li>
+                </ul>
+                </nav>
+
                 {html.children}
             </body>
         </html>
