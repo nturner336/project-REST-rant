ad1c8ef8df89dee49a6ce4122a57e1417f3aa0f7
Nichole Turner <nicholeturner@Nicholes-Air.lan>
2024-02-03 20:56:48 -0500

    add forms

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const NewForm = require('./NewForm'); // Adjust the import path accordingly
 
 let places = [
   {
@@ -17,10 +18,14 @@ let places = [
   }
 ];
 
+// GET /places/new
+router.get('/new', (req, res) => {
+  res.render('places/new'); // Renders the 'places/new' view
+});
+
 // GET /places
 router.get('/', (req, res) => {
-  // Render the 'places/index' view and pass the 'places' data as an object
-  res.render('places/index', { places });
+  res.render('places/index', { places }); // Renders the 'places/index' view with 'places' data
 });
 
 module.exports = router;
